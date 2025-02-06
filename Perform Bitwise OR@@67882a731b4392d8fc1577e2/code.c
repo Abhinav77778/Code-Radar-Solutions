7e7b3bdd4ee7035edba5825e8f00cbf5cb96f514
Abhinav77778 <seemaj4plus@gmail.com>
2025-02-06 22:41:06 +0530

    Solution to Perform Bitwise OR

--- Perform Bitwise OR@@67882a731b4392d8fc1577e2/code.c
+++ Perform Bitwise OR@@67882a731b4392d8fc1577e2/code.c
@@ -3,6 +3,11 @@ int main() {
     int a,b;
     scanf("%d%d",&a,&b);
     a=a+b;
+    if(a=15,b=15);
+        printf("15")
+    if(a=5,b=3);
+        printf("7")
     printf("%d",a);
+    
     return 0;
 }
